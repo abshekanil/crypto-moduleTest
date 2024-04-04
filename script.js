425947a66c299f45e31c93b1d264d4a6a13578b9
Abhishekanil007 <just4abhishekanil@gmail.com>
2024-04-04 14:21:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,593 @@
+let arr = [
+  {
+    id: "bitcoin",
+    symbol: "btc",
+    name: "Bitcoin",
+    image:
+      "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+    current_price: 65569,
+    market_cap: 1288530923678,
+    market_cap_rank: 1,
+    fully_diluted_valuation: 1375464400161,
+    total_volume: 32396419615,
+    high_24h: 66945,
+    low_24h: 65135,
+    price_change_24h: -545.038101562008,
+    price_change_percentage_24h: -0.82439,
+    market_cap_change_24h: -15978288268.1394,
+    market_cap_change_percentage_24h: -1.22485,
+    circulating_supply: 19672737,
+    total_supply: 21000000,
+    max_supply: 21000000,
+    ath: 73738,
+    ath_change_percentage: -11.12236,
+    ath_date: "2024-03-14T07:10:36.635Z",
+    atl: 67.81,
+    atl_change_percentage: 96548.72817,
+    atl_date: "2013-07-06T00:00:00.000Z",
+    roi: null,
+    last_updated: "2024-04-04T05:14:52.073Z",
+  },
+  {
+    id: "ethereum",
+    symbol: "eth",
+    name: "Ethereum",
+    image:
+      "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1696501628",
+    current_price: 3292.08,
+    market_cap: 394871752042,
+    market_cap_rank: 2,
+    fully_diluted_valuation: 394871752042,
+    total_volume: 14876030620,
+    high_24h: 3365.2,
+    low_24h: 3251.72,
+    price_change_24h: -14.1755941538431,
+    price_change_percentage_24h: -0.42875,
+    market_cap_change_24h: -3183001120.22974,
+    market_cap_change_percentage_24h: -0.79964,
+    circulating_supply: 120068372.18484,
+    total_supply: 120068372.18484,
+    max_supply: null,
+    ath: 4878.26,
+    ath_change_percentage: -32.57808,
+    ath_date: "2021-11-10T14:24:19.604Z",
+    atl: 0.432979,
+    atl_change_percentage: 759525.26837,
+    atl_date: "2015-10-20T00:00:00.000Z",
+    roi: {
+      times: 66.1445106241338,
+      currency: "btc",
+      percentage: 6614.45106241338,
+    },
+    last_updated: "2024-04-04T05:14:37.335Z",
+  },
+  {
+    id: "tether",
+    symbol: "usdt",
+    name: "Tether",
+    image:
+      "https://assets.coingecko.com/coins/images/325/large/Tether.png?1696501661",
+    current_price: 0.999598,
+    market_cap: 106111217717,
+    market_cap_rank: 3,
+    fully_diluted_valuation: 106111217717,
+    total_volume: 60101319436,
+    high_24h: 1.003,
+    low_24h: 0.995522,
+    price_change_24h: -0.000511053652384907,
+    price_change_percentage_24h: -0.0511,
+    market_cap_change_24h: 837713955,
+    market_cap_change_percentage_24h: 0.79575,
+    circulating_supply: 106149786847.074,
+    total_supply: 106149786847.074,
+    max_supply: null,
+    ath: 1.32,
+    ath_change_percentage: -24.40373,
+    ath_date: "2018-07-24T00:00:00.000Z",
+    atl: 0.572521,
+    atl_change_percentage: 74.70289,
+    atl_date: "2015-03-02T00:00:00.000Z",
+    roi: null,
+    last_updated: "2024-04-04T05:10:42.577Z",
+  },
+  {
+    id: "binancecoin",
+    symbol: "bnb",
+    name: "BNB",
+    image:
+      "https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1696501970",
+    current_price: 579.84,
+    market_cap: 89065359609,
+    market_cap_rank: 4,
+    fully_diluted_valuation: 89065359609,
+    total_volume: 2140034781,
+    high_24h: 579.03,
+    low_24h: 551.16,
+    price_change_24h: 23.44,
+    price_change_percentage_24h: 4.21213,
+    market_cap_change_24h: 3361497496,
+    market_cap_change_percentage_24h: 3.92222,
+    circulating_supply: 153856150,
+    total_supply: 153856150,
+    max_supply: 200000000,
+    ath: 686.31,
+    ath_change_percentage: -15.71027,
+    ath_date: "2021-05-10T07:24:17.097Z",
+    atl: 0.0398177,
+    atl_change_percentage: 1452736.40714,
+    atl_date: "2017-10-19T00:00:00.000Z",
+    roi: null,
+    last_updated: "2024-04-04T05:14:26.205Z",
+  },
+  {
+    id: "solana",
+    symbol: "sol",
+    name: "Solana",
+    image:
+      "https://assets.coingecko.com/coins/images/4128/large/solana.png?1696504756",
+    current_price: 182.42,
+    market_cap: 80954047961,
+    market_cap_rank: 5,
+    fully_diluted_valuation: 104319155959,
+    total_volume: 4507028923,
+    high_24h: 191.63,
+    low_24h: 180.32,
+    price_change_24h: -5.7339033298058,
+    price_change_percentage_24h: -3.04749,
+    market_cap_change_24h: -2656549242.04324,
+    market_cap_change_percentage_24h: -3.17729,
+    circulating_supply: 444813152.446094,
+    total_supply: 573195952.411258,
+    max_supply: null,
+    ath: 259.96,
+    ath_change_percentage: -29.9898,
+    ath_date: "2021-11-06T21:54:35.825Z",
+    atl: 0.500801,
+    atl_change_percentage: 36241.36306,
+    atl_date: "2020-05-11T19:35:23.449Z",
+    roi: null,
+    last_updated: "2024-04-04T05:14:44.453Z",
+  },
+  {
+    id: "usd-coin",
+    symbol: "usdc",
+    name: "USDC",
+    image:
+      "https://assets.coingecko.com/coins/images/6319/large/usdc.png?1696506694",
+    current_price: 1.001,
+    market_cap: 32895279071,
+    market_cap_rank: 6,
+    fully_diluted_valuation: 33045364771,
+    total_volume: 9752058553,
+    high_24h: 1.004,
+    low_24h: 0.995656,
+    price_change_24h: 0.00137942,
+    price_change_percentage_24h: 0.13803,
+    market_cap_change_24h: 30273360,
+    market_cap_change_percentage_24h: 0.09211,
+    circulating_supply: 32871250999.3446,
+    total_supply: 33021227070.3007,
+    max_supply: null,
+    ath: 1.17,
+    ath_change_percentage: -14.64956,
+    ath_date: "2019-05-08T00:40:28.300Z",
+    atl: 0.877647,
+    atl_change_percentage: 14.04474,
+    atl_date: "2023-03-11T08:02:13.981Z",
+    roi: null,
+    last_updated: "2024-04-04T05:14:26.215Z",
+  },
+  {
+    id: "staked-ether",
+    symbol: "steth",
+    name: "Lido Staked Ether",
+    image:
+      "https://assets.coingecko.com/coins/images/13442/large/steth_logo.png?1696513206",
+    current_price: 3286.19,
+    market_cap: 31324802600,
+    market_cap_rank: 7,
+    fully_diluted_valuation: 31324802600,
+    total_volume: 93927552,
+    high_24h: 3358.88,
+    low_24h: 3248.32,
+    price_change_24h: -20.7162988346845,
+    price_change_percentage_24h: -0.62646,
+    market_cap_change_24h: -324105592.94775,
+    market_cap_change_percentage_24h: -1.02407,
+    circulating_supply: 9554624.52794538,
+    total_supply: 9554624.52794538,
+    max_supply: null,
+    ath: 4829.57,
+    ath_change_percentage: -32.048,
+    ath_date: "2021-11-10T14:40:47.256Z",
+    atl: 482.9,
+    atl_change_percentage: 579.60567,
+    atl_date: "2020-12-22T04:08:21.854Z",
+    roi: null,
+    last_updated: "2024-04-04T05:14:29.707Z",
+  },
+  {
+    id: "ripple",
+    symbol: "xrp",
+    name: "XRP",
+    image:
+      "https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1696501442",
+    current_price: 0.569596,
+    market_cap: 31218807120,
+    market_cap_rank: 8,
+    fully_diluted_valuation: 56814007232,
+    total_volume: 1518649394,
+    high_24h: 0.592032,
+    low_24h: 0.56334,
+    price_change_24h: -0.0175535477400588,
+    price_change_percentage_24h: -2.98962,
+    market_cap_change_24h: -1065371736.06046,
+    market_cap_change_percentage_24h: -3.29998,
+    circulating_supply: 54942400126,
+    total_supply: 99987738355,
+    max_supply: 100000000000,
+    ath: 3.4,
+    ath_change_percentage: -83.2797,
+    ath_date: "2018-01-07T00:00:00.000Z",
+    atl: 0.00268621,
+    atl_change_percentage: 21053.65511,
+    atl_date: "2014-05-22T00:00:00.000Z",
+    roi: null,
+    last_updated: "2024-04-04T05:14:33.393Z",
+  },
+  {
+    id: "dogecoin",
+    symbol: "doge",
+    name: "Dogecoin",
+    image:
+      "https://assets.coingecko.com/coins/images/5/large/dogecoin.png?1696501409",
+    current_price: 0.175491,
+    market_cap: 25152624441,
+    market_cap_rank: 9,
+    fully_diluted_valuation: 25153373392,
+    total_volume: 2777756746,
+    high_24h: 0.187069,
+    low_24h: 0.171171,
+    price_change_24h: -0.0101944768579657,
+    price_change_percentage_24h: -5.49017,
+    market_cap_change_24h: -1579421498.00702,
+    market_cap_change_percentage_24h: -5.90834,
+    circulating_supply: 143738606383.705,
+    total_supply: 143743276383.705,
+    max_supply: null,
+    ath: 0.731578,
+    ath_change_percentage: -76.07617,
+    ath_date: "2021-05-08T05:08:23.458Z",
+    atl: 0.0000869,
+    atl_change_percentage: 201296.9701,
+    atl_date: "2015-05-06T00:00:00.000Z",
+    roi: null,
+    last_updated: "2024-04-04T05:14:48.695Z",
+  },
+  {
+    id: "cardano",
+    symbol: "ada",
+    name: "Cardano",
+    image:
+      "https://assets.coingecko.com/coins/images/975/large/cardano.png?1696502090",
+    current_price: 0.5708,
+    market_cap: 20055771920,
+    market_cap_rank: 10,
+    fully_diluted_valuation: 25585541286,
+    total_volume: 465710970,
+    high_24h: 0.59344,
+    low_24h: 0.561905,
+    price_change_24h: -0.0162867894753105,
+    price_change_percentage_24h: -2.77417,
+    market_cap_change_24h: -658435522.782169,
+    market_cap_change_percentage_24h: -3.17867,
+    circulating_supply: 35274209222.0096,
+    total_supply: 45000000000,
+    max_supply: 45000000000,
+    ath: 3.09,
+    ath_change_percentage: -81.64299,
+    ath_date: "2021-09-02T06:00:10.474Z",
+    atl: 0.01925275,
+    atl_change_percentage: 2843.29243,
+    atl_date: "2020-03-13T02:22:55.044Z",
+    roi: null,
+    last_updated: "2024-04-04T05:14:40.402Z",
+  },
+];
+
+let table = document.getElementById('myTable');
+let row1 = document.getElementById('row1');
+let col1 = document.getElementById('col1');
+let col2 = document.getElementById('col2');
+let col3 = document.getElementById('col3');
+let col4 = document.getElementById('col4');
+let col5 = document.getElementById('col5');
+let col6 = document.getElementById('col6');
+let col7 = document.getElementById('col7');
+
+let row2 = document.getElementById('row2');
+let col21 = document.getElementById('col21');
+let col22 = document.getElementById('col22');
+let col23 = document.getElementById('col23');
+let col24 = document.getElementById('col24');
+let col25 = document.getElementById('col25');
+let col26 = document.getElementById('col26');
+let col27 = document.getElementById('col27');
+
+let row3 = document.getElementById('row3');
+let col31 = document.getElementById('col31');
+let col32 = document.getElementById('col32');
+let col33 = document.getElementById('col33');
+let col34 = document.getElementById('col34');
+let col35 = document.getElementById('col35');
+let col36 = document.getElementById('col36');
+let col37 = document.getElementById('col37');
+
+let row4 = document.getElementById('row4');
+let col41 = document.getElementById('col41');
+let col42 = document.getElementById('col42');
+let col43 = document.getElementById('col43');
+let col44 = document.getElementById('col44');
+let col45 = document.getElementById('col45');
+let col46 = document.getElementById('col46');
+let col47 = document.getElementById('col47');
+
+let row5 = document.getElementById('row5');
+let col51 = document.getElementById('col51');
+let col52 = document.getElementById('col52');
+let col53 = document.getElementById('col53');
+let col54 = document.getElementById('col54');
+let col55 = document.getElementById('col55');
+let col56 = document.getElementById('col56');
+let col57 = document.getElementById('col57');
+
+let row6 = document.getElementById('row6');
+let col61 = document.getElementById('col61');
+let col62 = document.getElementById('col62');
+let col63 = document.getElementById('col63');
+let col64 = document.getElementById('col64');
+let col65 = document.getElementById('col65');
+let col66 = document.getElementById('col66');
+let col67 = document.getElementById('col67');
+
+let row7 = document.getElementById('row7');
+let col71 = document.getElementById('col71');
+let col72 = document.getElementById('col72');
+let col73 = document.getElementById('col73');
+let col74 = document.getElementById('col74');
+let col75 = document.getElementById('col75');
+let col76 = document.getElementById('col76');
+let col77 = document.getElementById('col77');
+
+let row8 = document.getElementById('row8');
+let col81 = document.getElementById('col81');
+let col82 = document.getElementById('col82');
+let col83 = document.getElementById('col83');
+let col84 = document.getElementById('col84');
+let col85 = document.getElementById('col85');
+let col86 = document.getElementById('col86');
+let col87 = document.getElementById('col87');
+
+let row9 = document.getElementById('row9');
+let col91 = document.getElementById('col91');
+let col92 = document.getElementById('col92');
+let col93 = document.getElementById('col93');
+let col94 = document.getElementById('col94');
+let col95 = document.getElementById('col95');
+let col96 = document.getElementById('col96');
+let col97 = document.getElementById('col97');
+
+let row10 = document.getElementById('row10');
+let col01 = document.getElementById('col01');
+let col02 = document.getElementById('col02');
+let col03 = document.getElementById('col03');
+let col04 = document.getElementById('col04');
+let col05 = document.getElementById('col05');
+let col06 = document.getElementById('col06');
+let col07 = document.getElementById('col07');
+
+
+function Filling(){
+    col1.innerHTML = `<img src="`+ arr[0].image + `" alt="img">`;
+    col2.innerHTML = arr[0].name;
+    col3.innerHTML = (arr[0].symbol).toUpperCase();
+    col4.innerHTML = `$` + arr[0].current_price;
+    col5.innerHTML = `$`+arr[0].fully_diluted_valuation;
+    col6.innerHTML = arr[0].market_cap_change_percentage_24h + "%";
+
+    let mktCap1 = parseFloat(arr[0].market_cap_change_percentage_24h);
+
+    if(mktCap1 > 0)
+    {
+        col6.style.color = "green";
+    }
+    else{
+        col6.style.color = "red";
+    }
+
+    col7.innerHTML = `Mkt Cap : $` + arr[0].market_cap;
+
+
+    col21.innerHTML = `<img src="`+ arr[1].image + `" alt="img">`;
+    col22.innerHTML = arr[1].name;
+    col23.innerHTML = (arr[1].symbol).toUpperCase();
+    col24.innerHTML = `$` + arr[1].current_price;
+    col25.innerHTML = `$`+arr[1].fully_diluted_valuation;
+    col26.innerHTML = arr[1].market_cap_change_percentage_24h + "%";
+
+    let mktCap2 = parseFloat(arr[1].market_cap_change_percentage_24h);
+
+    if(mktCap2 > 0)
+    {
+        col26.style.color = "green";
+    }
+    else{
+        col26.style.color = "red";
+    }
+    col27.innerHTML = `Mkt Cap : $` + arr[1].market_cap;
+
+
+    col31.innerHTML = `<img src="`+ arr[2].image + `" alt="img">`;
+    col32.innerHTML = arr[2].name;
+    col33.innerHTML = (arr[2].symbol).toUpperCase();
+    col34.innerHTML = `$` + arr[2].current_price;
+    col35.innerHTML = `$`+arr[2].fully_diluted_valuation;
+    col36.innerHTML = arr[2].market_cap_change_percentage_24h + "%";
+
+    let mktCap3 = parseFloat(arr[2].market_cap_change_percentage_24h);
+    if(mktCap3 > 0)
+    {
+        col36.style.color = "green";
+    }
+    else{
+        col36.style.color = "red";
+    }
+    col37.innerHTML = `Mkt Cap : $` + arr[2].market_cap;
+
+
+    col41.innerHTML = `<img src="`+ arr[3].image + `" alt="img">`;
+    col42.innerHTML = arr[3].name;
+    col43.innerHTML = (arr[3].symbol).toUpperCase();
+    col44.innerHTML = `$` + arr[3].current_price;
+    col45.innerHTML = `$`+arr[3].fully_diluted_valuation;
+    col46.innerHTML = arr[3].market_cap_change_percentage_24h + "%";
+
+    let mktCap4 = parseFloat(arr[3].market_cap_change_percentage_24h);
+    if(mktCap4 > 0)
+    {
+        col46.style.color = "green";
+    }
+    else{
+        col46.style.color = "red";
+    }
+    col47.innerHTML = `Mkt Cap : $` + arr[3].market_cap;
+
+
+    col51.innerHTML = `<img src="`+ arr[4].image + `" alt="img">`;
+    col52.innerHTML = arr[4].name;
+    col53.innerHTML = (arr[4].symbol).toUpperCase();
+    col54.innerHTML = `$` + arr[4].current_price;
+    col55.innerHTML = `$`+arr[4].fully_diluted_valuation;
+    col56.innerHTML = arr[4].market_cap_change_percentage_24h + "%";
+
+    let mktCap5 = parseFloat(arr[4].market_cap_change_percentage_24h);
+    if(mktCap5 > 0)
+    {
+        col56.style.color = "green";
+    }
+    else{
+        col56.style.color = "red";
+    }
+    col57.innerHTML = `Mkt Cap : $` + arr[4].market_cap;
+
+
+    col61.innerHTML = `<img src="`+ arr[5].image + `" alt="img">`;
+    col62.innerHTML = arr[5].name;
+    col63.innerHTML = (arr[5].symbol).toUpperCase();
+    col64.innerHTML = `$` + arr[5].current_price;
+    col65.innerHTML = `$`+arr[5].fully_diluted_valuation;
+    col66.innerHTML = arr[5].market_cap_change_percentage_24h + "%";
+
+    let mktCap6 = parseFloat(arr[5].market_cap_change_percentage_24h);
+    if(mktCap6 > 0)
+    {
+        col66.style.color = "green";
+    }
+    else{
+        col66.style.color = "red";
+    }
+    col67.innerHTML = `Mkt Cap : $` + arr[5].market_cap;
+
+    col71.innerHTML = `<img src="`+ arr[6].image + `" alt="img">`;
+    col72.innerHTML = arr[6].name;
+    col73.innerHTML = (arr[6].symbol).toUpperCase();
+    col74.innerHTML = `$` + arr[6].current_price;
+    col75.innerHTML = `$`+arr[6].fully_diluted_valuation;
+    col76.innerHTML = arr[6].market_cap_change_percentage_24h + "%";
+
+    let mktCap7 = parseFloat(arr[6].market_cap_change_percentage_24h);
+    if(mktCap7 > 0)
+    {
+        col76.style.color = "green";
+    }
+    else{
+        col76.style.color = "red";
+    }
+    col77.innerHTML = `Mkt Cap : $` + arr[6].market_cap;
+
+
+    col81.innerHTML = `<img src="`+ arr[7].image + `" alt="img">`;
+    col82.innerHTML = arr[7].name;
+    col83.innerHTML = (arr[7].symbol).toUpperCase();
+    col84.innerHTML = `$` + arr[7].current_price;
+    col85.innerHTML = `$`+arr[7].fully_diluted_valuation;
+    col86.innerHTML = arr[7].market_cap_change_percentage_24h + "%";
+
+    let mktCap8 = parseFloat(arr[7].market_cap_change_percentage_24h);
+    if(mktCap8 > 0)
+    {
+        col86.style.color = "green";
+    }
+    else{
+        col86.style.color = "red";
+    }
+    col87.innerHTML = `Mkt Cap : $` + arr[7].market_cap;
+
+
+    col91.innerHTML = `<img src="`+ arr[8].image + `" alt="img">`;
+    col92.innerHTML = arr[8].name;
+    col93.innerHTML = (arr[8].symbol).toUpperCase();
+    col94.innerHTML = `$` + arr[8].current_price;
+    col95.innerHTML = `$`+arr[8].fully_diluted_valuation;
+    col96.innerHTML = arr[8].market_cap_change_percentage_24h + "%";
+
+    let mktCap9 = parseFloat(arr[8].market_cap_change_percentage_24h);
+    if(mktCap9 > 0)
+    {
+        col96.style.color = "green";
+    }
+    else{
+        col96.style.color = "red";
+    }
+    col97.innerHTML = `Mkt Cap : $` + arr[8].market_cap;
+
+    col01.innerHTML = `<img src="`+ arr[9].image + `" alt="img">`;
+    col02.innerHTML = arr[9].name;
+    col03.innerHTML = (arr[9].symbol).toUpperCase();
+    col04.innerHTML = `$` + arr[9].current_price;
+    col05.innerHTML = `$`+arr[9].fully_diluted_valuation;
+    col06.innerHTML = arr[9].market_cap_change_percentage_24h + "%";
+
+    let mktCap10 = parseFloat(arr[9].market_cap_change_percentage_24h);
+    if(mktCap10 > 0)
+    {
+        col06.style.color = "green";
+    }
+    else{
+        col06.style.color = "red";
+    }
+    col07.innerHTML = `Mkt Cap : $` + arr[9].market_cap;
+}
+
+
+Filling();
+
+
+
+
+
+
+fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    // Process the fetched data here
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
